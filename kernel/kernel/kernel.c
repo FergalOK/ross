3acc1007c7f45973902f4c93f12500b25a60b6e1
fergal <fergal.okane@yahoo.com>
2023-05-21 16:29:26 +0200

    Create first page table

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <kernel/tty.h>
 
 void kernel_main(void) {
 	terminal_initialize();
+
+	// This cause page fault :)
+	// int* ptr = (int*)0xdeadbeaf;
+	// *ptr = 0x41;
+
 	printf("Hello, kernel World!\n");
 }
